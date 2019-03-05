5386726fb0d9fab988334f8941d2701367a3419b
Rémy Oudompheng <remy@archlinux.org>
2019-03-05 23:12:35 +0100

    Reorganize Python code by introducing a Rat class

--- fptest.py
+++ fptest.py
@@ -123,14 +123,14 @@ def find_hard_parse(e2, e10, mantbits=54, prec=96):
     (± 1e16 / 2**(96+53)) which yields about 5000 candidates.
     """
     if e2 < prec:
-        n1, d1 = approx(2**prec - 1, 10**e10 * 2**(prec-e2), bound=2**mantbits)
-        n2, d2 = approx(2**prec + 1, 10**e10 * 2**(prec-e2), bound=2**mantbits)
+        r1 = Rat(2**prec - 1, 10**e10 * 2**(prec-e2), bound=2**mantbits)
+        r2 = Rat(2**prec + 1, 10**e10 * 2**(prec-e2), bound=2**mantbits)
     else:
-        n1, d1 = approx(2**e2 - 2**(e2-prec), 10**e10, bound=2**mantbits)
-        n2, d2 = approx(2**e2 + 2**(e2-prec), 10**e10, bound=2**mantbits)
+        r1 = Rat(2**e2 - 2**(e2-prec), 10**e10, bound=2**mantbits)
+        r2 = Rat(2**e2 + 2**(e2-prec), 10**e10, bound=2**mantbits)
     #print("bounds 2**{}/10**{}: {}/{} -> {}/{}".format(
     #    e2, e10, n1, d1, n2, d2))
-    for x, y in walk(n1, d1, n2, d2, bound=2**mantbits):
+    for x, y in walk(r1, r2, bound=2**mantbits):
         digs, mant = x, y
         # try odd multiples
         if mant % 2 == 1:
@@ -163,13 +163,13 @@ def find_hard_parse_negexp(e2, e10, mantbits=54, prec=96, denormal=False):
     """
     if e10 < prec:
         # multiply by 2**(prec-e10)
-        n1, d1 = approx(10**e10 * 2**(prec-e10) - 5**e10, 2**(e2+prec-e10), bound=2**mantbits)
-        n2, d2 = approx(10**e10 * 2**(prec-e10) + 5**e10, 2**(e2+prec-e10), bound=2**mantbits)
+        r1 = Rat(10**e10 * 2**(prec-e10) - 5**e10, 2**(e2+prec-e10), bound=2**mantbits)
+        r2 = Rat(10**e10 * 2**(prec-e10) + 5**e10, 2**(e2+prec-e10), bound=2**mantbits)
     else:
-        n1, d1 = approx(10**e10 - (10**e10 >> prec), 2**e2, bound=2**mantbits)
-        n2, d2 = approx(10**e10 + (10**e10 >> prec), 2**e2, bound=2**mantbits)
+        r1 = Rat(10**e10 - (10**e10 >> prec), 2**e2, bound=2**mantbits)
+        r2 = Rat(10**e10 + (10**e10 >> prec), 2**e2, bound=2**mantbits)
 
-    for x, y in walk(n1, d1, n2, d2, bound=2**mantbits):
+    for x, y in walk(r1, r2, bound=2**mantbits):
         digs, mant = x, y
         # try odd multiples
         if mant % 2 == 1:
@@ -201,14 +201,14 @@ def find_hard_print(e2, e10, mantbits=53, prec=96):
     BOUND = 2**(1+mantbits)
 
     if e2 < prec:
-        n1, d1 = approx(2**prec - 1, 10**e10 * 2**(prec-e2), bound=BOUND)
-        n2, d2 = approx(2**prec + 1, 10**e10 * 2**(prec-e2), bound=BOUND)
+        r1 = Rat(2**prec - 1, 10**e10 * 2**(prec-e2), bound=BOUND)
+        r2 = Rat(2**prec + 1, 10**e10 * 2**(prec-e2), bound=BOUND)
     else:
-        n1, d1 = approx(2**e2 - 2**(e2-prec), 10**e10, bound=BOUND)
-        n2, d2 = approx(2**e2 + 2**(e2-prec), 10**e10, bound=BOUND)
+        r1 = Rat(2**e2 - 2**(e2-prec), 10**e10, bound=BOUND)
+        r2 = Rat(2**e2 + 2**(e2-prec), 10**e10, bound=BOUND)
     #print("bounds 2**{}/10**{}: {}/{} -> {}/{}".format(
     #    e2, e10, n1, d1, n2, d2))
-    for x, y in walk(n1, d1, n2, d2, bound=BOUND):
+    for x, y in walk(r1, r2, bound=BOUND):
         digs, mant = x, y
         # try odd multiples
         if mant & 1 == 0 and digs & 1 == 1:
@@ -233,13 +233,13 @@ def find_hard_print_negexp(e2, e10, mantbits=53, prec=96, denormal=False):
 
     if e10 < prec:
         # multiply by 2**(prec-e10)
-        n1, d1 = approx(10**e10 * 2**(prec-e10) - 5**e10, 2**(e2+prec-e10), bound=BOUND)
-        n2, d2 = approx(10**e10 * 2**(prec-e10) + 5**e10, 2**(e2+prec-e10), bound=BOUND)
+        r1 = Rat(10**e10 * 2**(prec-e10) - 5**e10, 2**(e2+prec-e10), bound=BOUND)
+        r2 = Rat(10**e10 * 2**(prec-e10) + 5**e10, 2**(e2+prec-e10), bound=BOUND)
     else:
-        n1, d1 = approx(10**e10 - (10**e10 >> prec), 2**e2, bound=BOUND)
-        n2, d2 = approx(10**e10 + (10**e10 >> prec), 2**e2, bound=BOUND)
+        r1 = Rat(10**e10 - (10**e10 >> prec), 2**e2, bound=BOUND)
+        r2 = Rat(10**e10 + (10**e10 >> prec), 2**e2, bound=BOUND)
 
-    for x, y in walk(n1, d1, n2, d2, bound=BOUND):
+    for x, y in walk(r1, r2, bound=BOUND):
         digs, mant = x, y
         # try odd multiples
         if mant & 1 == 0 and digs & 1 == 1:
@@ -257,190 +257,230 @@ def find_hard_print_negexp(e2, e10, mantbits=53, prec=96, denormal=False):
                 d += 2*digs
 
 
-def walk(x1, y1, x2, y2, bound):
+def walk(r1, r2, bound):
     """
-    Walk enumerates fractions between x1/y2 and x2/y2
+    Walk enumerates fractions between r1 and r2
     for a given denominator bound (the Farey sequence).
 
-    >>> list(walk(1, 4, 1, 2, bound=8))
+    >>> list(walk(Rat(1, 4), Rat(1, 2), bound=8))
     [(1, 4), (2, 7), (1, 3), (3, 8), (2, 5), (3, 7), (1, 2)]
 
     In this example, the tree is:
                 ___  1/3 (0,3) ____
     1/4 (0, 4) ´                   ` 2/5 (0,2,2)
        ` 2/7 (0,3,2)  3/8 (0,2,1,2)´             ` 3/7 (0,2,3)
+
+    >>> list(walk(Rat(1, 4), Rat(1, 3), bound=12))
+    [(1, 4), (3, 11), (2, 7), (3, 10), (1, 3)]
+    >>> list(walk(Rat(1, 4), Rat(2, 7), bound=32))
+    [(1, 4), (8, 31), (7, 27), (6, 23), (5, 19), (4, 15), (7, 26), (3, 11), (8, 29), (5, 18), (7, 25), (9, 32), (2, 7)]
+    >>> len(list(walk(Rat(65352703432539, 79006570561214),
+    ...          Rat(34807131698651, 42079240217226), bound=2**48)))
+    39930
     """
-    start = expand_cont_frac(x1, y1)
-    end = expand_cont_frac(x2, y2)
-    l = start
-    yield x1, y1
-    while l != end:
-        lr = right(l.copy())
-        num, den = to_fraction(lr)
+
+    # FIXME: a couple of duplicates seem to appear
+    l = r1.clone()
+    yield l.fraction()
+    while l.fraction() != r2.fraction():
+        l = l.next(bound)
+        yield l.fraction()
+
+class Rat:
+    def __init__(self, num, den, bound=None):
+        """
+        Computes a continued fraction expansion of a / b.
+        If bound is not None, the expansion stops at the
+        largest denominator <= bound.
+
+        >>> Rat(89, 55).cont
+        [1, 1, 1, 1, 1, 1, 1, 1, 2]
+        >>> Rat(355, 113).cont
+        [3, 7, 16]
+        >>> Rat(30, 20).cont
+        [1, 2]
+
+        >>> Rat(89, 55, bound=20).fraction()
+        (21, 13)
+        >>> Rat(355, 113, bound=200).fraction()
+        (355, 113)
+        >>> Rat(355, 113, bound=100).fraction()
+        (22, 7)
+        """
+        self.cont = []
+        # Invariant: (p*a + q*b, r*a + s*b)
+        p, q = 1, 0
+        r, s = 0, 1
+        a, b = num, den
+        while b > 1:
+            quo = a // b
+            rem = a % b
+            a, b = b, rem
+            # (p*quo + q)*b + p*rem = p*a + q*b
+            p, q = p*quo + q, p
+            r, s = r*quo + s, r
+            if bound is not None and r > bound:
+                return
+            self.cont.append(quo)
+
+        if b == 1:
+            p, q = p*a + q, p
+            r, s = r*a + s, r
+            if bound is not None and r > bound:
+                return
+            self.cont.append(a)
+
+    def __eq__(self, other):
+        if not isinstance(other, Rat):
+            return NotImplemented
+        return self.cont == other.cont
+
+    def __neq__(self, other):
+        if not isinstance(other, Rat):
+            return NotImplemented
+        return self.cont != other.cont
+
+    @staticmethod
+    def from_expansion(l):
+        r = Rat(0, 1)
+        r.cont = l
+        return r
+
+    def clone(self):
+        return Rat.from_expansion(self.cont.copy())
+
+    def fraction(self):
+        """
+        >>> Rat.from_expansion([3, 7, 16]).fraction()
+        (355, 113)
+        >>> Rat.from_expansion([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]).fraction()
+        (89, 55)
+        """
+        a, b = 1, 0
+        for q in reversed(self.cont):
+            a, b = a*q + b, a
+        return a, b
+
+    def child(self, i):
+        """
+        Sets self to its left (i=0) or right (i=1) child
+        in the Stern-Brocot tree.
+
+        The children of (a0, ..., an) in the tree are:
+            (a0, ..., an+1)
+            (a0, ..., an-1, 2)
+
+        >>> r = Rat.from_expansion([0, 3])
+        >>> r.child(1).cont
+        [0, 2, 2]
+        """
+        expansion = self.cont
+        if len(expansion) % 2 == i:
+            expansion[-1] += 1
+        else:
+            expansion[-1] -= 1
+            expansion.append(2)
+        self.cont = expansion
+        return self
+
+    def child_fraction(self, i):
+        """
+        self.child_fraction(i) == self.child(i).fraction()
+
+        >>> r = Rat(123456, 456789)
+        >>> r.child_fraction(0) == r.clone().child(0).fraction()
+        True
+        >>> r.child_fraction(1) == r.clone().child(1).fraction()
+        True
+        """
+        a, b = 1, 0
+        c, d = 0, 1
+        for idx in range(len(self.cont)-1):
+            a, b = a * self.cont[idx] + b, a
+            c, d = c * self.cont[idx] + d, c
+        if len(self.cont) % 2 == i:
+            # last + 1
+            q = self.cont[-1] + 1
+            a, b = a * q + b, a
+            c, d = c * q + d, c
+        else:
+            # (last - 1, 2)
+            q = self.cont[-1] - 1
+            a, b = a * q + b, a
+            c, d = c * q + d, c
+            a, b = a * 2 + b, a
+            c, d = c * 2 + d, c
+        return a, c
+
+    def next_up(self):
+        """
+        next_up returns the next element in the Stern-Brocot tree
+        which is not a child.
+
+        That is, next_up(N) is the nearest ancestor A of N
+        such that N is a child of A->left
+
+        >>> Rat.from_expansion([1, 1, 1, 2]).next_up().cont
+        [1, 1, 2]
+        >>> Rat.from_expansion([1, 1, 1, 1, 2]).next_up().cont
+        [1, 1, 2]
+        >>> Rat.from_expansion([1, 1, 1, 1, 256]).next_up().cont
+        [1, 1, 2]
+        >>> Rat.from_expansion([1, 1, 1, 1, 256123456789]).next_up().cont
+        [1, 1, 2]
+        >>> Rat.from_expansion([2]).next_up().cont
+        [3]
+        >>> Rat.from_expansion([0, 3, 2]).next_up().cont
+        [0, 3]
+        """
+
+        exp = self.cont
+        if len(exp) == 1:
+            # exception for integers
+            exp = [exp[0]+1]
+        elif len(exp) % 2 == 0:
+            #                     (..k+1)
+            #          ..(..k, 2)´
+            # (..k, n)´
+            last = exp[-1]
+            if last > 2:
+                exp[-1] -= 1
+            else:
+                exp = exp[:-1]
+                exp[-1] += 1
+        else:
+            # parent obtained by decrementing n = exp[-1]
+            # is always smaller. Let's assume n=2
+            #         _______________(..k)
+            # (..k+1)´
+            #    `(..k, 2)
+            #            `... (..k, n)
+            exp = exp[:-1]
+            if exp[-1] == 1:
+                # normalize
+                exp = exp[:-1]
+                exp[-1] += 1
+
+        self.cont = exp
+        return self
+
+    def next(self, bound):
+        """
+        The next item in the Farey sequence of bound bound.
+        """
+        # Peek right child
+        num, den = self.child_fraction(1)
         if den > bound:
-            l = next_up(l)
-            yield to_fraction(l)
+            return self.next_up()
         else:
             # go down the left branch
-            l = lr
+            self.child(1)
             while True:
-                l2 = left(l.copy())
-                n2, d2 = to_fraction(l2)
+                n2, d2 = self.child_fraction(0)
                 if d2 <= bound:
-                    l, num, den = l2, n2, d2
+                    self.child(0)
                 else:
-                    break
-            yield num, den
-
-def expand_cont_frac(a, b):
-    """
-    Computes a continued fraction expansion of a / b.
-    The result is a list of integers.
-
-    >>> expand_cont_frac(89, 55)
-    [1, 1, 1, 1, 1, 1, 1, 1, 2]
-    >>> expand_cont_frac(355, 113)
-    [3, 7, 16]
-    >>> expand_cont_frac(30, 20)
-    [1, 2]
-    """
-    exp = []
-    while b > 1:
-        q = a // b
-        exp.append(q)
-        r = a % b
-        a, b = b, r
-
-    if b == 1:
-        exp.append(a)
-
-    return exp
-
-def approx(a, b, bound):
-    """
-    Computes a good approximation to a / b
-    within the provided denominator bound.
-
-    This is not necessarily the best approximation for that bound.
-
-    >>> approx(89, 55, 20)
-    (21, 13)
-    >>> approx(355, 113, 200)
-    (355, 113)
-    >>> approx(355, 113, 100)
-    (22, 7)
-    """
-    # Invariant: (p*a + q*b, r*a + s*b)
-    p, q = 1, 0
-    r, s = 0, 1
-
-    while b > 1:
-        quo = a // b
-        rem = a % b
-        a, b = b, rem
-        # (p*quo + q)*b + p*rem = p*a + q*b
-        p, q = p*quo + q, p
-        r, s = r*quo + s, r
-        if r > bound:
-            return q, s
-
-    if b == 1:
-        p, q = p*a + q, p
-        r, s = r*a + s, r
-        if r > bound:
-            return q, s
-
-    return p, r
-
-def to_fraction(expansion):
-    """
-    >>> to_fraction([3, 7, 16])
-    (355, 113)
-    >>> to_fraction([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    (89, 55)
-    """
-    a, b = 1, 0
-    for q in reversed(expansion):
-        a, b = a*q + b, a
-    return a, b
-
-def left(expansion):
-    if len(expansion) % 2 == 0:
-        expansion[-1] += 1
-    else:
-        expansion[-1] -= 1
-        expansion.append(2)
-    return expansion
-
-def right(expansion):
-    """
-    next returns the right child in the Stern-Brocot tree.
-    The argument may be mutated.
-
-    The children of (a0, ..., an) in the tree are:
-        (a0, ..., an+1)
-        (a0, ..., an-1, 2)
-
-    >>> right([0, 3])
-    [0, 2, 2]
-    """
-    if len(expansion) % 2 == 1:
-        expansion[-1] += 1
-    else:
-        expansion[-1] -= 1
-        expansion.append(2)
-    return expansion
-
-def next_up(expansion):
-    """
-    next_up returns the next element in the Stern-Brocot tree
-    which is not a child.
-
-    That is, next_up(N) is the nearest ancestor A of N
-    such that N is a child of A->left
-
-    >>> next_up([1, 1, 1, 2])
-    [1, 1, 2]
-    >>> next_up([1, 1, 1, 1, 2])
-    [1, 1, 2]
-    >>> next_up([1, 1, 1, 1, 256])
-    [1, 1, 2]
-    >>> next_up([1, 1, 1, 1, 256123456789])
-    [1, 1, 2]
-    >>> next_up([2])
-    [3]
-    >>> next_up([0, 3, 2])
-    [0, 3]
-    """
-
-    exp = expansion
-    if len(exp) == 1:
-        return [exp[0]+1]
-    elif len(exp) % 2 == 0:
-        #                     (..k+1)
-        #          ..(..k, 2)´
-        # (..k, n)´
-        last = exp[-1]
-        if last > 2:
-            exp[-1] -= 1
-            return exp
-        else:
-            exp = exp[:-1]
-            exp[-1] += 1
-            return exp
-    else:
-        # parent obtained by decrementing n = exp[-1]
-        # is always smaller. Let's assume n=2
-        #         _______________(..k)
-        # (..k+1)´
-        #    `(..k, 2)
-        #            `... (..k, n)
-        exp = exp[:-1]
-        if exp[-1] == 1:
-            # normalize
-            exp = exp[:-1]
-            exp[-1] += 1
-        return exp
+                    return self
 
 if __name__ == "__main__":
     main()
